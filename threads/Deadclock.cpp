b134335de3ccce4d667681e445a09e7fd89b5836
whybfq <13588119112@163.com>
2018-08-06 14:02:06 +0800

    Update Deadclock.cpp

--- threads/Deadclock.cpp
+++ threads/Deadclock.cpp
@@ -50,16 +50,11 @@ Output:
 2. Acquiring m1
 
 
-/*History:
+/*modify History2:
    1.Date: Aug 6, 2018
      Author: San
      Modification: Easier
 */
-#include <iostream>
-#include <thread>
-#include <vector>
-using namespace std;
-
 int main() {
     std::mutex m1;
     thread t1([&m1] {
@@ -79,3 +74,33 @@ int main() {
 Output:
 1. Acquiring m1.2. Acquiring m1
 
+/*modify History 3:
+   1.Date: Aug 6, 2018
+     Author: San
+     Modification: more clearly
+*/
+int main() {
+    mutex m1;
+
+    thread t3( [&m1] {
+        cout << "This is threadt3, " ;
+        cout << &m1  << endl;
+    });
+
+
+    thread t1([&m1] {
+        cout << "1. Acquiring m1." << endl;
+        m1.lock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    });
+    thread t2([&m1] {
+        cout << "2. Acquiring m1" << endl;
+        m1.lock();
+    });
+
+
+    t1.join();
+    t2.join();
+    t3.join();  //按照说明的顺序已经先执行
+}
+This is threadt3, 1. Acquiring m1.2. Acquiring m10x7ffee89ef988
