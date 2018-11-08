4ac29672797e1a37d8993677cefd90fe5ede352e
whybfq <13588119112@163.com>
2018-11-08 16:30:53 +0800

    Update simpleTest.cpp

--- Input/cin/simpleTest.cpp
+++ Input/cin/simpleTest.cpp
@@ -1,3 +1,34 @@
+/*********************************************************************************
+  * @copyright (C)  2018 sanner All Rights Reserved
+  * @license: GPL
+  * @version:
+  * @data:    Nov 2018
+  * @file:
+  * @related: http://www.sohu.com/a/120517509_427866
+  * @brief:   The aim of this file is to test cin()
+  * @bug :
+  * @author:  sanner
+  *
+  * @CMAKE_CXX_STANDARD 14
+  * @IDE:     Clion 2018.2
+  * @OS:      macOS 10.14
+  *
+  * @function_lists:
+  *  1.       void inputTest()    --   test cin()
+  * @Revision:
+     1.@date:
+       @author:
+       @modification: Original
+     2.@date:
+       @author:
+       @modification:
+**********************************************************************************/
+#include <iostream>
+using std::cin ;
+using std::cout;
+using std::string;
+using std::endl;
+
 void testInput() {
     string str;
     cout << "cin test " << endl;
@@ -18,4 +49,8 @@ void inputTest2() {
     cout << ch1 << endl;
     cout << ch2 << endl;
     cout << (int)ch1 << " " << (int)ch2 << endl;
+}
+
+int main(int argc, char* argv[]) {
+    inputTest2();
 }
