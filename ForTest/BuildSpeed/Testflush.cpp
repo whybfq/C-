3e48c1631ed6f696f9cdc796f557c10e538c42a4
whybfq <13588119112@163.com>
2018-11-09 10:55:36 +0800

    Create Testflush.cpp

---
+++ ForTest/BuildSpeed/Testflush.cpp
@@ -0,0 +1,54 @@
+/*********************************************************************************
+  * @copyright (C)  2018 sanner All Rights Reserved
+  * @license: GPL
+  * @version:
+  * @data:    Nov 2018
+  * @file:    Testflush.cpp
+  * @related: https://en.cppreference.com/w/cpp/io/manip/flush
+  * @brief:   
+  * @bug :    
+  * @author:  sanner
+  *
+  * @CMAKE_CXX_STANDARD 14
+  * @IDE:     Clion 2018.2
+  * @OS:      macOS 10.14
+  *
+  * @function_lists:
+  *  1. flush() :
+  *     template<class CharT, class Traits >
+        std::basic_ostream<CharT , Traits>& flush(std::basic_ostream<CharT, Traits>& os);
+  * @Revision:
+     1.@date:
+       @author:
+       @modification: 
+     2.@date:
+       @author:
+       @modification:
+**********************************************************************************/
+#include <iostream>
+#include <chrono>
+
+template<typename Diff>
+void log_progress(Diff d)
+{
+    std::cout << "..("
+              << std::chrono::duration_cast<std::chrono::milliseconds>(d).count()
+              << " ms).." << std::flush;
+}
+
+int main(int argc, char* argv[]) {
+    volatile int sink = 0;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 10000; ++j) {
+            for (int k = 0; k < 20000; ++k) {
+                sink += k * j;
+            }
+        }
+        auto now = std::chrono::high_resolution_clock::now();
+        log_progress(now - t1);
+    }
+    std::cout << std::endl;
+}
+
