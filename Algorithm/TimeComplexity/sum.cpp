bba714477ce63d637561967bb5450b0955c72570
whybfq <13588119112@163.com>
2018-12-29 21:33:11 +0700

    Update sum.cpp

--- Algorithm/TimeComplexity/sum.cpp
+++ Algorithm/TimeComplexity/sum.cpp
@@ -24,3 +24,34 @@ int main(int argc, char* argv[]) {
     sumtime = e - s;
     cout << "time: " << sumtime  << endl;
 }
+#include <iostream>
+#include <cmath>  // use pow
+#include <ctime>
+
+using std:: cout;
+using std:: endl;
+
+const double n = 1e8; // you can change the number later
+
+int sum1(int n) {  // the time complexity is n, is too large
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += int(pow(-1,i));
+    }
+    return sum;
+}
+
+int sum2(int n) {
+    if(n % 2 == 0) return 0;
+    else return -1;
+}
+
+int main(int argc, char* argv[]) {
+    time_t s, e,sumtime;
+    s = clock();
+//    cout << "sum1:" << sum1(n) << endl;
+    cout << "sum2:" << sum2(n) << endl;
+    e = clock();
+    sumtime = e - s;
+    cout << "time: " << sumtime  << endl;
+}
