055af75588a2d40e4a3c1d03226047d831047a2c
whybfq <13588119112@163.com>
2018-12-30 17:50:47 +0700

    Update Fibonacci.cpp

--- Algorithm/SpaceComplexity/Fibonacci.cpp
+++ Algorithm/SpaceComplexity/Fibonacci.cpp
@@ -1,12 +1,16 @@
-/*********************************************************************************
+
+
+
+    /*********************************************************************************
   * @copyright (C)  2018 sanner All Rights Reserved
-  * @license: GPL
+  * @license: GP
   * @version:
   * @data:    Dec 2018
-  * @file:    Fibonaci.cpp
+  * @file:    sum.cpp
   * @related:
-  * @brief:   a good algorithm should be high efficiency, low storage, small O(f(n)),
-  *           compare the time between two algorithms
+  * @reference: https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
+  * @brief:     a good algorithm should be high efficiency, low storage, small O(f(n)),
+  *             compare the time between two algorithms
   * @bug :
   * @author:  sanner
   *
@@ -14,7 +18,7 @@
   * @IDE:     Clion 2018.3
   * @OS:      macOS 10.14
   *
-  * @function_lists:    Fist: Time complexity    Space complexity
+  * @function_lists:
   *  1.
   * @Revision:
      1.@date:
@@ -29,14 +33,12 @@ using std:: cin;
 using std:: cout;
 using std:: endl;
 
-
 // O(2^n)  O(n)
 long long Fib1(int n) {
     if(n < 1) return -1;
     else if(n == 1 || n == 2) return 1;
     else return Fib1(n - 1) + Fib1(n - 2);
 }
-
 
 // O(n)  O(n)
 long double Fib2(int n) { // Save previous values to avoid heavy double counting
@@ -53,6 +55,20 @@ long double Fib2(int n) { // Save previous values to avoid heavy double counting
     return temp;
 }
 
+// the improvement of Fib(2)   O(n)  O(1)
+long double Fib3(int n) {
+    int i, s1, s2;
+    if(n < 1) return -1;
+    if(n == 1 || n == 2) return 1;
+    s1 = 1;
+    s2 = 1;
+    for(i = 3; i <= n; ++i ) {
+        s2 = s1 + s2;  // Twisting and adding
+        s1 = s2 - s1;  // Record the previous item
+    }
+    return s2;
+}
+
 int main(int argc, char* argv[]) {
     time_t start, e, sumtime;
     int n;
@@ -60,7 +76,8 @@ int main(int argc, char* argv[]) {
     start = clock();
     // cout << fixed << serp
 //    cout << "Fib1("<<n<<")=" << Fib1(n) << endl;
-    cout << "Fib2("<<n<<")=" << Fib2(n) << endl;
+//    cout << "Fib2("<<n<<")=" << Fib2(n) << endl;
+    cout << "Fib3("<<n<<")=" << Fib3(n) << endl;
     e = clock();
     sumtime = e - start;
     cout << "time: " << sumtime << endl;
